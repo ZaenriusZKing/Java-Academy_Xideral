758658e0393f01efeb9a440d03eddec2d6364738
ZaenriusZKing <161345027+ZaenriusZKing@users.noreply.github.com>
2024-09-07 20:20:29 -0600

    Delete semana 4 directory

--- semana 4/SpringBatch/src/main/java/SpringBatch/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package SpringBatch.repository;
-
-import SpringBatch.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
--- semana 4/SpringRESTAPISpringDataJPACRUD/src/main/java/SpringRESTAPISpringDataJPACRUD/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package SpringRESTAPISpringDataJPACRUD.repository;
-
-import SpringRESTAPISpringDataJPACRUD.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
