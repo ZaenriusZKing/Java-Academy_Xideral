fb48a1dbb2652b1393f881f30971cd6277fed422
ZaenriusZKing <161345027+ZaenriusZKing@users.noreply.github.com>
2024-09-17 16:10:09 -0600

    Delete Semana FINAL directory

--- Semana FINAL/HospitalEmployeeManager/src/main/java/com/hospitalApp/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospitalApp.dao;
-
-import com.hospitalApp.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
